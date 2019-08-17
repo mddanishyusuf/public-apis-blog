247c2898bd2863b13d026fca73cb23a05614553d
mddanishyusuf <mddanishyusuf@gmail.com>
2019-08-17 15:33:59 +0530

    favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,19 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-sass`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+            },
+        },
         {
             resolve: 'gatsby-source-apiserver',
             options: {
